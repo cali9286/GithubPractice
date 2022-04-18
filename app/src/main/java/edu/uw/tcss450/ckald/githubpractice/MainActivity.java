da895d8cbe26dea1a8629aa966931a69d5bb60ef
Foziya99 <fozi@uw.edu>
2022-04-18 13:36:01 -0700

    3:07

--- app/src/main/java/edu/uw/tcss450/ckald/githubpractice/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/ckald/githubpractice/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     //edited by foziya
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
